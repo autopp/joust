00fa66f7cb057e2dd423408134f83551d9f20fb9
autopp <autopp.inc@gmail.com>
2017-10-14 20:15:14 +0900

    genarate action of tournament

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -3,5 +3,6 @@
 #
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+get '/tournament', to: 'tournament#index'
 
 root to: 'tournament#index'
---
+++ apps/web/views/tournament/index.rb
@@ -0,0 +1,5 @@
+module Web::Views::Tournament
+  class Index
+    include Web::View
+  end
+end
