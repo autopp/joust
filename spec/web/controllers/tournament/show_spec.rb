3d93b6bcbe3563db38d12588c71172c6f76e6afd
autopp <autopp.inc@gmail.com>
2017-10-31 09:44:20 +0900

    spec: add spec for exposures of Tournament::Show

--- spec/web/controllers/tournament/show_spec.rb
+++ spec/web/controllers/tournament/show_spec.rb
@@ -1,11 +1,23 @@
 require_relative '../../../../apps/web/controllers/tournament/show'
 
 RSpec.describe Web::Controllers::Tournament::Show do
-  let(:action) { described_class.new }
+  let(:action) { described_class.new(repo: repo) }
   let(:params) { Hash[id: 1] }
+
+  let(:tournament) { Tournament.new(id: 1, name: 'My Tournament') }
+  let(:repo) do
+    repo = double('repo')
+    allow(repo).to receive(:find).with(id: 1).and_return(tournament)
+    repo
+  end
 
   it 'is successful' do
     response = action.call(params)
     expect(response[0]).to eq 200
+  end
+
+  it 'exposes tournament' do
+    action.call(params)
+    expect(action.exposures).to include(tournament: tournament)
   end
 end
