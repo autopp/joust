27e76fe6ddc5a80bedc8019b30636f88ce2e3ad0
autopp <autopp.inc@gmail.com>
2017-11-02 09:42:55 +0900

    routes: add routing for new tournament page

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -4,6 +4,7 @@
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 get '/tournament/:id', to: 'tournament#show', as: :tournament
+get '/tournament/new', to: 'tournament#new', as: :new_tournament
 get '/tournaments', to: 'tournament#index', as: :tournaments
 
 root to: 'tournament#index'
--- spec/web/routes_spec.rb
+++ spec/web/routes_spec.rb
@@ -12,4 +12,8 @@ describe Web.routes do
   it 'generates "/tournament/1"' do
     expect(described_class.path(:tournament, id: 1)).to eq('/tournament/1')
   end
+
+  it 'generates "/tournament/new"' do
+    expect(described_class.path(:new_tournament)).to eq('/tournament/new')
+  end
 end
