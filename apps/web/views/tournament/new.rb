defe3d4d871a9f9754d26f5394cb095e29ff8654
autopp <autopp.inc@gmail.com>
2017-11-03 19:20:57 +0900

    view: impl Tournament::New#title, #back_path

--- apps/web/views/tournament/new.rb
+++ apps/web/views/tournament/new.rb
@@ -1,5 +1,13 @@
 module Web::Views::Tournament
   class New
     include Web::View
+
+    def title
+      'New Tournament'
+    end
+
+    def back_path
+      Web.routes.path(:tournaments)
+    end
   end
 end
--- spec/web/views/tournament/new_spec.rb
+++ spec/web/views/tournament/new_spec.rb
@@ -1,12 +1,10 @@
 require_relative '../../../../apps/web/views/tournament/new'
 
 RSpec.describe Web::Views::Tournament::New do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { {} }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/tournament/new.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    expect(view.foo).to eq exposures.fetch(:foo)
-  end
+  it_behaves_like 'common view', title: 'New Tournament', back_path: Web.routes.path(:tournaments)
 end
