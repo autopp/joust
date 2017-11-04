aa5ec1d630bb86c1bcf93cf48794f2aaac224e26
autopp <autopp.inc@gmail.com>
2017-11-05 01:31:59 +0900

    routes: fix to recognize '/tournaments/new'

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -4,7 +4,7 @@
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 get '/tournaments', to: 'tournament#index', as: :tournaments
-get '/tournaments/:id', to: 'tournament#show', as: :tournament
 get '/tournaments/new', to: 'tournament#new', as: :new_tournament
+get '/tournaments/:id', to: 'tournament#show', as: :tournament
 
 root to: 'tournament#index'
