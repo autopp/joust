698d9499fccc807812fafea116682273a1fdc9cd
autopp <autopp.inc@gmail.com>
2017-11-05 16:26:57 +0900

    routes: fix order

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -5,7 +5,7 @@
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 get '/tournaments', to: 'tournament#index', as: :tournaments
 get '/tournaments/new', to: 'tournament#new', as: :new_tournament
+post '/tournaments', to: 'tournament#create', as: :tournaments
 get '/tournaments/:id', to: 'tournament#show', as: :tournament
 
 root to: 'tournament#index'
-post '/tournament', to: 'tournament#create'
