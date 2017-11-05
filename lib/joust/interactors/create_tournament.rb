5a1f2ce69af45967ed65a0057ac13e373df64948
autopp <autopp.inc@gmail.com>
2017-11-06 01:44:49 +0900

    interactor: impl CreateTournament for valid case

--- lib/joust/interactors/create_tournament.rb
+++ lib/joust/interactors/create_tournament.rb
@@ -4,10 +4,17 @@ module Interactors
   class CreateTournament
     include Hanami::Interactor
 
-    def initialize
+    expose :tournament
+
+    def initialize(params, repo: TournamentRepository.new)
+      @params = params
+      @player_names = params[:players].to_s.each_line.map(&:chomp).reject(&:empty?)
+      @repo = repo
     end
 
     def call
+      args = @params.to_h.merge!(player_names: @player_names)
+      @tournament = @repo.create_with_players(args)
     end
   end
 end
--- spec/joust/interactors/create_tournament_spec.rb
+++ spec/joust/interactors/create_tournament_spec.rb
@@ -1,7 +1,34 @@
 describe Interactors::CreateTournament do
-  let(:interactor) { described_class.new }
+  let(:interactor) { described_class.new(params, repo: repo) }
+  let(:repo) { double('TournamentRepository') }
+  let(:name) { 'My Tournament' }
 
-  it 'succeeds' do
-    expect(interactor.call).to be_a_success
+  context 'when parameters are valid' do
+    subject { interactor.call }
+
+    let(:params) do
+      {
+        name: name, players: "player1\nplayer2\nplayer3\nplayer4\n",
+        total_vp_used: '0', rank_history_used: '1'
+      }
+    end
+
+    let(:tournament) do
+      Tournament.new(id: 1, name: name)
+    end
+
+    before do
+      args = including(
+        name: name, player_names: %w[player1 player2 player3 player4],
+        total_vp_used: '0', rank_history_used: '1'
+      )
+      expect(repo).to receive(:create_with_players).with(args).once.and_return(tournament)
+    end
+
+    it { is_expected.to be_a_success }
+
+    it 'exposes the tournament created' do
+      expect(subject.tournament).to eq(tournament)
+    end
   end
 end
