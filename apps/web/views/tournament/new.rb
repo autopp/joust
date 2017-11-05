6542b069f908260ec06d2f7da74955991a9876ac
autopp <autopp.inc@gmail.com>
2017-11-05 16:10:29 +0900

    view: impl Tournament::New#form

--- apps/web/views/tournament/new.rb
+++ apps/web/views/tournament/new.rb
@@ -9,5 +9,35 @@ module Web::Views::Tournament
     def back_path
       Web.routes.path(:tournaments)
     end
+
+    def form
+      form_for :tournament, '/tournaments', class: 'center-block' do
+        div class: 'row' do
+          div class: "col-md-4 col-md-offset-4 center" do
+            text_area :players, '', rows: 25, cols: 20, placeholder: "input names per line"
+          end
+        end
+
+        div class: 'row' do
+          div class: "col-md-4 col-md-offset-4 center" do
+            check_box :total_vp_used, checked_value: true, unchecked_value: 'false'
+            label :total_vp_used
+          end
+        end
+
+        div class: 'row' do
+          div class: "col-md-4 col-md-offset-4 center" do
+            check_box :rank_history_used, checked_value: true, unchecked_value: 'false'
+            label :rank_history_used
+          end
+        end
+
+        div class: 'row' do
+          div class: "col-md-4 col-md-offset-4 center" do
+            submit 'Create', class: 'btn btn-primary'
+          end
+        end
+      end
+    end
   end
 end
--- spec/web/views/tournament/new_spec.rb
+++ spec/web/views/tournament/new_spec.rb
@@ -1,10 +1,22 @@
 require_relative '../../../../apps/web/views/tournament/new'
 
 RSpec.describe Web::Views::Tournament::New do
-  let(:exposures) { {} }
+  let(:exposures) { { params: {} } }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/tournament/new.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
   it_behaves_like 'common view', title: 'New Tournament', back_path: Web.routes.path(:tournaments)
+
+  describe '#form' do
+    subject { view.form.to_s }
+
+    it 'generates form to create a tournament' do
+      expect(subject).to start_with('<form') & end_with('</form>')
+    end
+
+    it 'is included in rendered' do
+      expect(rendered).to include(subject)
+    end
+  end
 end
