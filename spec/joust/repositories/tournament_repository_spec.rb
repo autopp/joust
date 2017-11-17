25b6c001a2deb6b805c999c17d0601afe763f75a
autopp <autopp.inc@gmail.com>
2017-11-17 09:49:25 +0900

    model: add spec for TournamentRepository#create_with_players

--- spec/joust/repositories/tournament_repository_spec.rb
+++ spec/joust/repositories/tournament_repository_spec.rb
@@ -1,3 +1,20 @@
-RSpec.describe TournamentRepository do
-  # place your tests here
+describe TournamentRepository do
+  let(:repo) { described_class.new }
+
+  describe '#create_with_players' do
+    subject do
+      repo.create_with_players(
+        name: 'My Tournament', total_vp_used: true, rank_history_used: true,
+        player_names: %[player1, player2, player3, player4]
+      )
+    end
+
+    it 'returns created tournament players' do
+      expected = {
+        name: 'My Tournament', total_vp_used: true, rank_history_used: true,
+        players: []
+      }
+      expect(subject.to_h).to include(expected)
+    end
+  end
 end
