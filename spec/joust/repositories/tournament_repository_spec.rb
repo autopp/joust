fdfc6bebe97659b3965927ca1945541046477d82
autopp <autopp.inc@gmail.com>
2017-11-20 09:44:01 +0900

    model: change interface of TournamentRepository#create_with_players

--- spec/joust/repositories/tournament_repository_spec.rb
+++ spec/joust/repositories/tournament_repository_spec.rb
@@ -2,22 +2,25 @@ describe TournamentRepository do
   let(:repo) { described_class.new }
 
   describe '#create_with_players' do
+    let(:name) { 'My Tournament' }
+    let(:players) do
+      [{ name: 'player1' }, { name: 'player2' }, { name: 'player3' }, { name: 'player4' }]
+    end
+
     subject do
       repo.create_with_players(
-        name: 'My Tournament', total_vp_used: true, rank_history_used: true,
-        player_names: %w[player1 player2 player3 player4]
+        name: name, total_vp_used: true, rank_history_used: true, players: players
       )
     end
 
-    it 'returns created tournament players' do
+    it 'returns created tournament with players' do
       pending
       expected = {
-        name: 'My Tournament', total_vp_used: true, rank_history_used: true,
-        players: [
-          { name: 'player1' }, { name: 'player2' }, { name: 'player3' }, { name: 'player4' }
-        ]
+        name: 'My Tournament', total_vp_used: true, rank_history_used: true, finished_count: 0
       }
       expect(subject.to_h).to include(expected)
+      expect(subject.players.map(&:name)).to eq(%w[player1 player2 player3 player4])
+      expect(subject.players.map(&:tournament_id)).to all(eq(subject.id))
     end
   end
 end
