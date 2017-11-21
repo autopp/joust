a683716b551318953253b72dd41c6a667b138bfc
autopp <autopp.inc@gmail.com>
2017-11-21 09:41:53 +0900

    action: add skeleton of test case for Tournament::Create

--- spec/web/controllers/tournament/create_spec.rb
+++ spec/web/controllers/tournament/create_spec.rb
@@ -2,8 +2,13 @@ RSpec.describe Web::Controllers::Tournament::Create, type: :action do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  context 'when parameters are valid' do
+    it 'redirects to "/tournaments/1"'
+
+    it 'exposes the created tournament'
+  end
+
+  context 'when parameters are invalid' do
+    it 'is failure'
   end
 end
