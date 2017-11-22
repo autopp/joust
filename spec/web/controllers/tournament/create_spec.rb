04f6bfefea0fb5d771f8ee747b69d03b6ba187c4
autopp <autopp.inc@gmail.com>
2017-11-22 09:39:48 +0900

    action: change spec of Tournament::Create

--- spec/web/controllers/tournament/create_spec.rb
+++ spec/web/controllers/tournament/create_spec.rb
@@ -1,9 +1,31 @@
 RSpec.describe Web::Controllers::Tournament::Create, type: :action do
-  let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:action) { described_class.new(interactor: interactor) }
+  let(:interactor) { instance_double(Interactors::CreateTournament) }
+  let(:name) { 'My Tournament' }
+
+  let(:params) do
+    { name: name, players: players, total_vp_used: '0', rank_history_used: '1' }
+  end
 
   context 'when parameters are valid' do
-    it 'redirects to "/tournaments/1"'
+    let(:name) { 'My Tournament' }
+    let(:players) { "player2\nplayer1\nplayer3\nplayer4\n" }
+    let(:tournament) do
+      Tournament.new(
+        id: 1, name: name, total_vp_used: false, rank_history_used: true, finished_count: 0,
+        players: (1..4).map { |i| Player.new(id: i, name: "player#{i}") }
+      )
+    end
+
+    before do
+      allow(interactor).to receive(:call).with(params).and_return(tournament)
+    end
+
+    it 'redirects to "/tournaments/1"' do
+      response = action.call(params)
+      expect(response[0]).to eq(302)
+      expect(response[1]).to include('Location' => '/tournaments/1')
+    end
 
     it 'exposes the created tournament'
   end
