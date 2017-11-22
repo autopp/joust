2ce4f4fb29bfd8d51d00f4ae90223c171dea0fdf
autopp <autopp.inc@gmail.com>
2017-11-22 09:52:11 +0900

    action: impl Tournament::Create for valid params

--- apps/web/controllers/tournament/create.rb
+++ apps/web/controllers/tournament/create.rb
@@ -2,7 +2,14 @@ module Web::Controllers::Tournament
   class Create
     include Web::Action
 
+    def initialize(interactor: Interactors::Tournament.new)
+      @interactor = interactor
+    end
+
     def call(params)
+      result = @interactor.call(params)
+      @tournament = result.tournament
+      redirect_to routes.path(:tournament, id: @tournament.id)
     end
   end
 end
