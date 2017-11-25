9a2b10f80ed6ad9fe083814c9db7b0456ce66ab8
autopp <autopp.inc@gmail.com>
2017-11-25 11:57:01 +0900

    view: extract common method for Tournament::New & Tournament::Create

--- apps/web/views/tournament/create.rb
+++ apps/web/views/tournament/create.rb
@@ -1,6 +1,8 @@
 module Web::Views::Tournament
   class Create
     include Web::View
+
+    include Web::Views::NewTournament
     template 'tournament/new'
   end
 end
--- spec/web/views/tournament/create_spec.rb
+++ spec/web/views/tournament/create_spec.rb
@@ -1,6 +1,23 @@
+require_relative '../views_helper.rb'
+require_relative '../../../../apps/web/views/tournament/create'
+
 RSpec.describe Web::Views::Tournament::Create, type: :view do
-  let(:exposures) { Hash[foo: 'bar'] }
-  let(:template)  { Hanami::View::Template.new('apps/web/templates/tournament/create.html.erb') }
+  let(:exposures) { { params: {} } }
+  let(:template)  { Hanami::View::Template.new('apps/web/templates/tournament/new.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
+
+  it_behaves_like 'common view', title: 'New Tournament', back_path: Web.routes.path(:tournaments)
+
+  describe '#form' do
+    subject { view.form.to_s }
+
+    it 'generates form to create a tournament' do
+      expect(subject).to start_with('<form') & end_with('</form>')
+    end
+
+    it 'is included in rendered' do
+      expect(rendered).to include(subject)
+    end
+  end
 end
