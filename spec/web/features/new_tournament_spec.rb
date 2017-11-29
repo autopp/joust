427f460484a0138ff2a8635e6b7cc414ce09424c
autopp <autopp.inc@gmail.com>
2017-11-29 09:53:01 +0900

    spec: add creating tournament test at valid case

--- spec/web/features/new_tournament_spec.rb
+++ spec/web/features/new_tournament_spec.rb
@@ -6,4 +6,20 @@ feature 'Create New Tournament' do
     click_on 'New Tournament'
     expect(current_path).to eq('/tournaments/new')
   end
+
+  scenario 'Create tournament by valid input' do
+    players = <<~INPUT
+      player1
+      player2
+      player3
+      player4
+    INPUT
+
+    visit Web.routes.path(:new_tournament)
+    fill_in 'tournament-name', with: 'My Tournament'
+    fill_in 'tournament-players', with: players
+
+    click_on 'Create'
+    expect(current_path).to match(%r{/tournaments/\d+})
+  end
 end
