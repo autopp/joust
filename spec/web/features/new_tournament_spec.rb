fbda93031b46eea583a85ef870c5cffb4a6ea7cf
autopp <autopp.inc@gmail.com>
2017-11-30 09:51:24 +0900

    feature: add scenario about create tournament

--- spec/web/features/new_tournament_spec.rb
+++ spec/web/features/new_tournament_spec.rb
@@ -22,4 +22,52 @@ feature 'Create New Tournament' do
     click_on 'Create'
     expect(current_path).to match(%r{/tournaments/\d+})
   end
+
+  scenario 'Create tournament by valid input' do
+    players = <<~INPUT
+      player1
+      player2
+      player3
+      player4
+    INPUT
+
+    visit Web.routes.path(:new_tournament)
+    fill_in 'tournament-name', with: 'My Tournament'
+    fill_in 'tournament-players', with: players
+
+    click_on 'Create'
+    expect(current_path).to match(%r{/tournaments/\d+})
+    expect(page).not_to have_css('div', id: 'error_explanation')
+  end
+
+  scenario 'Create tournament by invalid input (invalid number of players)' do
+    players = <<~INPUT
+      player1
+      player2
+    INPUT
+
+    visit Web.routes.path(:new_tournament)
+    fill_in 'tournament-name', with: 'My Tournament'
+    fill_in 'tournament-players', with: players
+
+    click_on 'Create'
+    expect(current_path).not_to match(%r{/tournaments/\d+})
+    expect(page).to have_css('div', id: 'error_explanation')
+  end
+
+  scenario 'Create tournament by invalid input (missing tournament name)' do
+    players = <<~INPUT
+      player1
+      player2
+      player3
+      player4
+    INPUT
+
+    visit Web.routes.path(:new_tournament)
+    fill_in 'tournament-players', with: players
+
+    click_on 'Create'
+    expect(current_path).not_to match(%r{/tournaments/\d+})
+    expect(page).to have_css('div', id: 'error_explanation')
+  end
 end
