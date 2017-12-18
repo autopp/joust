8fe17a32b35a58b3d128485ffe81b6e8d08dfe91
autopp <autopp.inc@gmail.com>
2017-12-18 09:33:45 +0900

    db: add column of rounds

--- db/migrations/20171217132959_create_rounds.rb
+++ db/migrations/20171217132959_create_rounds.rb
@@ -2,9 +2,12 @@ Hanami::Model.migration do
   change do
     create_table :rounds do
       primary_key :id
+      foreign_key :tournament_id, :tournaments, on_delete: :cascade, null: false
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
+
+      column :number, Integer, null: false
     end
   end
 end
