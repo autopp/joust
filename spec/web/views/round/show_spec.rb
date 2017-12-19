fcf9897801375664f88049eab2393c767e538cff
autopp <autopp.inc@gmail.com>
2017-12-19 21:50:55 +0900

    view: add common case of Round::Show

--- spec/web/views/round/show_spec.rb
+++ spec/web/views/round/show_spec.rb
@@ -1,9 +1,16 @@
+require_relative '../views_helper.rb'
+require_relative '../../../../apps/web/views/round/show'
+
 RSpec.describe Web::Views::Round::Show, type: :view do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) do
+    {}
+  end
   let(:template)  { Hanami::View::Template.new('apps/web/templates/round/show.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
+  back_path = Web.routes.path(:round, tournament_id: 1, number: 2)
+  it_behaves_like 'common view', title: 'New Tournament', back_path: back_path do
+    before { pending }
   end
 end
