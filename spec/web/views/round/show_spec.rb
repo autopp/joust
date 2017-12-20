8e79729b5766621d8b0078e6e620d9554c25e8c9
autopp <autopp.inc@gmail.com>
2017-12-20 09:43:03 +0900

    view: fix spec of Round::Show

--- spec/web/views/round/show_spec.rb
+++ spec/web/views/round/show_spec.rb
@@ -3,14 +3,18 @@ require_relative '../../../../apps/web/views/round/show'
 
 RSpec.describe Web::Views::Round::Show, type: :view do
   let(:exposures) do
-    {}
+    {
+      tournament: Tournament.new(id: 1, name: 'My Tournament'),
+      round: Round.new(id: 1, number: 2)
+    }
   end
+
   let(:template)  { Hanami::View::Template.new('apps/web/templates/round/show.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  back_path = Web.routes.path(:round, tournament_id: 1, number: 2)
-  it_behaves_like 'common view', title: 'New Tournament', back_path: back_path do
+  back_path = Web.routes.path(:tournament, id: 1)
+  it_behaves_like 'common view', title: 'My Tournament: Round 2', back_path: back_path do
     before { pending }
   end
 end
