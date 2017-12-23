5d86efd31e1573fd2146097dbac4efb87a664060
autopp <autopp.inc@gmail.com>
2017-12-23 11:37:29 +0900

    model: add spec of Score#tp & Score#vp

--- spec/joust/entities/score_spec.rb
+++ spec/joust/entities/score_spec.rb
@@ -1,3 +1,20 @@
 RSpec.describe Score, type: :entity do
-  # place your tests here
+  let(:score) do
+    described_class.new(
+      player_count: 3, vp_gained: 30, has_extra_turn: true,
+      rank: 1, same_ranks: 2
+    )
+  end
+
+  describe '#vp' do
+    subject { score.vp }
+
+    xit { is_expected.to eq(30 * Rational(3, 4)) }
+  end
+
+  describe '#tp' do
+    subject { score.tp }
+
+    xit { is_expected.to(Rational(6 + 3, 2)) }
+  end
 end
