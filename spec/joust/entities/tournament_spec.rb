da35ee657b2f0d8987631ecf8e08831bc8031209
autopp <autopp.inc@gmail.com>
2017-12-26 09:45:24 +0900

    model: add test case of Tournament#ranking

--- spec/joust/entities/tournament_spec.rb
+++ spec/joust/entities/tournament_spec.rb
@@ -1,3 +1,25 @@
-RSpec.describe Tournament do
-  # place your tests here
+RSpec.describe Tournament, players: 7 do
+  let(:tournament) { described_class.new(finished_count: finished_count, players: players) }
+
+  describe '#ranking' do
+    subject { tournament.ranking }
+
+    context 'when finished round is one' do
+      let(:finished_count) { 1 }
+
+      it 'returns ranking from scores of round 1' do
+        expect(subject).to eq(
+          [
+            [1, player1],
+            [2, player5],
+            [3, player2],
+            [4, player6],
+            [5, player3],
+            [6, player7],
+            [7, player4]
+          ]
+        )
+      end
+    end
+  end
 end
