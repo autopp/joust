62f7782ba28988198bad17bef8839468712ce7b4
autopp <autopp.inc@gmail.com>
2017-12-27 09:40:46 +0900

    model: impl Tournament#ranking

--- lib/joust/entities/tournament.rb
+++ lib/joust/entities/tournament.rb
@@ -1,2 +1,11 @@
 class Tournament < Hanami::Entity
+  def ranking
+    ranking_values = players.map do |player|
+      v = [player.scores.map(&:tp).reduce(&:+)]
+      v << player.scores.map(&:vp).reduce(&:+) if total_vp_used
+      [v, player]
+    end
+
+    ranking_values.sort.map.with_index { |(_, p), i| [i + 1, p] }
+  end
 end
