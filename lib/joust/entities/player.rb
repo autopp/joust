346ca1fce7c0c1b8ed96975a6036cf27dce2ef8c
autopp <autopp.inc@gmail.com>
2018-01-04 18:35:15 +0900

    model: impl Player#ranking_value

--- lib/joust/entities/player.rb
+++ lib/joust/entities/player.rb
@@ -1,2 +1,9 @@
 class Player < Hanami::Entity
+  def ranking_value
+    scores.each_with_object([0, 0, [0, 0, 0, 0]]) do |score, value|
+      value[0] += score.tp
+      value[1] += score.vp
+      value[2][score.rank - 1] += 1
+    end
+  end
 end
