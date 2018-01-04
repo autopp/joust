36177ffe845a9666286c831bef2fbfce8cf5243f
autopp <autopp.inc@gmail.com>
2018-01-04 16:22:16 +0900

    model: add test cases for Tournament::ranking

--- spec/joust/entities/tournament_spec.rb
+++ spec/joust/entities/tournament_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Tournament, players: 7 do
   let(:tournament) do
     described_class.new(
       finished_count: finished_count, players: players,
-      total_vp_used: true, vp_history_used: true
+      total_vp_used: total_vp_used, rank_history_used: rank_history_used
     )
   end
 
@@ -11,6 +11,8 @@ RSpec.describe Tournament, players: 7 do
 
     context 'when finished round is one' do
       let(:finished_count) { 1 }
+      let(:total_vp_used) { true }
+      let(:rank_history_used) { true }
 
       it 'returns ranking from scores of round 1' do
         expect(subject).to eq(
@@ -29,6 +31,86 @@ RSpec.describe Tournament, players: 7 do
 
     context 'when finished round is two' do
       let(:finished_count) { 2 }
+
+      context 'when total vp is used' do
+        let(:total_vp_used) { true }
+
+        context 'when rank history is used' do
+          let(:rank_history_used) { true }
+
+          it 'returns ranking from scores of round 2' do
+            expect(subject).to eq(
+              [
+                { rank: 1, player: player1 },
+                { rank: 2, player: player5 },
+                { rank: 3, player: player4 },
+                { rank: 4, player: player2 },
+                { rank: 5, player: player3 },
+                { rank: 6, player: player7 },
+                { rank: 7, player: player6 }
+              ]
+            )
+          end
+        end
+
+        context 'when rank history is not used' do
+          let(:rank_history_used) { false }
+
+          it 'returns ranking from scores of round 2' do
+            expect(subject).to eq(
+              [
+                { rank: 1, player: player1 },
+                { rank: 2, player: player5 },
+                { rank: 3, player: player2 },
+                { rank: 3, player: player4 },
+                { rank: 5, player: player3 },
+                { rank: 6, player: player6 },
+                { rank: 6, player: player7 }
+              ]
+            )
+          end
+        end
+      end
+
+      context 'when total vp is not used' do
+        let(:total_vp_used) { false }
+
+        context 'when rank history is used' do
+          let(:rank_history_used) { true }
+
+          it 'returns ranking from scores of round 2' do
+            expect(subject).to eq(
+              [
+                { rank: 1, player: player1 },
+                { rank: 2, player: player5 },
+                { rank: 3, player: player4 },
+                { rank: 4, player: player2 },
+                { rank: 5, player: player3 },
+                { rank: 5, player: player7 },
+                { rank: 7, player: player6 }
+              ]
+            )
+          end
+        end
+
+        context 'when rank history is not used' do
+          let(:rank_history_used) { false }
+
+          it 'returns ranking from scores of round 2' do
+            expect(subject).to eq(
+              [
+                { rank: 1, player: player1 },
+                { rank: 2, player: player5 },
+                { rank: 3, player: player2 },
+                { rank: 3, player: player4 },
+                { rank: 5, player: player3 },
+                { rank: 5, player: player6 },
+                { rank: 5, player: player7 }
+              ]
+            )
+          end
+        end
+      end
     end
   end
 end
