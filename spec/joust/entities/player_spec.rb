6110e85b22e54b510a63f9181a4f7229e8226537
autopp <autopp.inc@gmail.com>
2018-01-04 18:32:25 +0900

    model: add spec of Player#ranking_value

--- spec/joust/entities/player_spec.rb
+++ spec/joust/entities/player_spec.rb
@@ -1,3 +1,11 @@
-RSpec.describe Player, type: :entity do
-  # place your tests here
+RSpec.describe Player, type: :entity, players: 7 do
+  let(:player) { player1 }
+
+  describe '#ranking_value' do
+    subject { player.ranking_value }
+
+    it 'returns tuple of tp, vp and rank history' do
+      expect(subject).to eq([12 / 1r, 70 / 1r, [2, 0, 0, 0]])
+    end
+  end
 end
