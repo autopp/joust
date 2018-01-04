e525ff960545b85c7bbe633ddae1ba63e1c75a4d
autopp <autopp.inc@gmail.com>
2018-01-04 18:25:33 +0900

    model: impl Tournament#ranking

--- lib/joust/entities/tournament.rb
+++ lib/joust/entities/tournament.rb
@@ -4,14 +4,15 @@ class Tournament < Hanami::Entity
       scores = player.scores.take(finished_count)
       v = [scores.map(&:tp).reduce(&:+)]
       v << scores.map(&:vp).reduce(&:+) if total_vp_used
+      if rank_history_used
+        v << scores.each_with_object(Array.new(4, 0)) { |s, counts| counts[s.rank - 1] += 1 }
+      end
       v
     end
 
     rank = 1
     ranking_values.sort_by(&:first).reverse.each_with_object([]) do |(_, players), r|
-      players.each do |p|
-        r << { rank: rank, player: p }
-      end
+      players.each { |p| r << { rank: rank, player: p } }
       rank += players.size
     end
   end
