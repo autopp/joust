06066803ae58d7e1aff699324d2063f827d348b2
autopp <autopp.inc@gmail.com>
2018-01-12 09:54:40 +0900

    model: impl RoundRepository#find_with_scores_by_tournament_id

--- lib/joust/repositories/round_repository.rb
+++ lib/joust/repositories/round_repository.rb
@@ -1,5 +1,10 @@
 class RoundRepository < Hanami::Repository
   associations do
+    belongs_to :tournament
     has_many :scores
+  end
+
+  def find_with_scores_by_tournament_id(tournament_id)
+    aggregate(:tournament, :scores).where(tournament_id: tournament_id).map_to(Round).to_a
   end
 end
