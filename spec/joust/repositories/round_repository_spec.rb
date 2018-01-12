ba8130c7b2b5678c637d8c7b4a67151b4b6b1e6b
autopp <autopp.inc@gmail.com>
2018-01-12 09:51:06 +0900

    model: add spec of RoundRepository#find_with_scores_by_tournament_id

--- spec/joust/repositories/round_repository_spec.rb
+++ spec/joust/repositories/round_repository_spec.rb
@@ -1,3 +1,39 @@
 RSpec.describe RoundRepository, type: :repository do
-  # place your tests here
+  let(:repo) { described_class.new }
+
+  describe '#find_with_scores_by_tournament_id', players: 7 do
+    subject { repo.find_with_scores_by_tournament_id(1) }
+
+    let(:tournament) do
+      Tournament.new(
+        id: 1, name: 'My Tournament', finished_count: 0, players: players, rounds: rounds
+      )
+    end
+
+    before do
+      TournamentRepository.new.create(tournament.to_h)
+
+      round_repo = RoundRepository.new
+      rounds.each { |r| round_repo.create(r.to_h) }
+
+      player_repo = PlayerRepository.new
+      score_repo = ScoreRepository.new
+      players.each do |p|
+        player_repo.create(p.to_h)
+        p.scores.each { |s| score_repo.create(s.to_h) }
+      end
+    end
+
+    it 'returns rounds with scores' do
+      expect(subject).to eq(rounds)
+      expected_scores = rounds.map(&:scores).map do |scores|
+        scores.map do |score|
+          a_hash_including(created_at: a_value, updated_at: a_value, **score.to_h)
+        end
+      end
+
+      actual_scores = subject.map(&:scores).map { |scores| scores.map(&:to_h) }
+      expect(actual_scores).to match(expected_scores)
+    end
+  end
 end
