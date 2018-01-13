bef939f8b10578b51eded79a95affa9cf057ace0
autopp <autopp.inc@gmail.com>
2018-01-13 22:53:28 +0900

    interactor: impl FindTournament

--- lib/joust/interactors/find_tournament.rb
+++ lib/joust/interactors/find_tournament.rb
@@ -4,8 +4,29 @@ module Interactors
   class FindTournament
     include Hanami::Interactor
 
+    expose :tournament
+
+    def initialize(
+      tournament_repo: TournamentRepository.new,
+      player_repo: PlayerRepository.new, round_repo: RoundRepository.new
+    )
+      @tournament_repo = tournament_repo
+      @player_repo = player_repo
+      @round_repo = round_repo
+    end
+
     def call(params)
-      error('not implemented')
+      id = params[:id]
+      tournament = @tournament_repo.find(id)
+      unless tournament
+        error("Tournament #{id} is not found")
+        return
+      end
+
+      players = @player_repo.find_with_scores_by_tournament_id(tournament_id: id)
+      rounds = @round_repo.find_with_scores_by_tournament_id(tournament_id: id)
+
+      @tournament = Tournament.new(tournament.to_h.merge(players: players, rounds: rounds))
     end
   end
 end
