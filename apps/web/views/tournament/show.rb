5ce1e57ede06dfa3210790efc0210827b7f9d3f4
autopp <autopp.inc@gmail.com>
2018-01-15 09:44:52 +0900

    view: impl ranking table header for /tournament/:id

--- apps/web/views/tournament/show.rb
+++ apps/web/views/tournament/show.rb
@@ -9,5 +9,27 @@ module Web::Views::Tournament
     def back_path
       Web.routes.path(:tournaments)
     end
+
+    def ranking_table
+      html.table(class: 'table table-bordered') do
+        ranking_header
+      end
+    end
+
+    def ranking_header
+      html.thead do
+        tr do
+          td 'Rank', rowspan: '2'
+          td 'Name', rowspan: '2', colspan: '2'
+          1.upto(tournament.finished_count) do |i|
+            td colspan: '2' do
+              link = Web.routes.path(:round, tournament_id: tournament.id, number: i)
+              link_to("Round #{i}", link, id: i)
+            end
+          end
+          td 'Total', colspan: '2'
+        end
+      end
+    end
   end
 end
