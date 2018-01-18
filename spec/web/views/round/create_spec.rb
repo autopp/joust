6c8ae6166dc4fa777a6916be6656bf3fda3020a6
autopp <autopp.inc@gmail.com>
2018-01-18 09:42:40 +0900

    view: delete auto generated assert

--- spec/web/views/round/create_spec.rb
+++ spec/web/views/round/create_spec.rb
@@ -3,11 +3,4 @@ RSpec.describe Web::Views::Round::Create, type: :view do
   let(:template)  { Hanami::View::Template.new('apps/web/templates/round/create.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
-
-  it 'exposes #foo' do
-    pending 'This is an auto-generated test. Edit it and add your own tests.'
-
-    # Example
-    expect(view.foo).to eq exposures.fetch(:foo)
-  end
 end
