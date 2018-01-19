b6dd439878773186f8cb97d69c0d1e7d25d83642
autopp <autopp.inc@gmail.com>
2018-01-19 21:17:58 +0900

    view: add spec of Tournament::Show#round_link

--- spec/web/views/tournament/show_spec.rb
+++ spec/web/views/tournament/show_spec.rb
@@ -23,4 +23,26 @@ RSpec.describe Web::Views::Tournament::Show, players: 7 do
       expect(subject.to_s).to have_tag('table')
     end
   end
+
+  describe '#round_link' do
+    subject { view.round_link }
+
+    context 'when ongoring round exists' do
+      let(:finished_count) { 1 }
+
+      it 'contains link to ongoing round page' do
+        path = Web.routes.path(:round, tournament_id: 1, number: 2)
+        expect(subject.to_s).to have_tag('a', with: { aref: path })
+      end
+    end
+
+    context 'when ongoring round exists' do
+      let(:finished_count) { 2 }
+
+      it 'contains link to ongoing round page' do
+        path = Web.routes.path(:rounds, tournament_id: 1)
+        expect(subject.to_s).to have_tag('a', with: { aref: path })
+      end
+    end
+  end
 end
