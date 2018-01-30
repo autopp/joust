eed5dfedd9fd0a38e640e2ce5b9e308bfd8a06de
autopp <autopp.inc@gmail.com>
2018-01-30 09:50:16 +0900

    fix: use string for `describe` to output readability

--- spec/web/routes_spec.rb
+++ spec/web/routes_spec.rb
@@ -1,34 +1,36 @@
 require 'spec_helper'
 
-describe Web.routes do
+describe 'Web.routes' do
+  let(:routes) { Web.routes }
+
   it 'generates "/"' do
-    expect(described_class.path(:root)).to eq('/')
+    expect(routes.path(:root)).to eq('/')
   end
 
   it 'generates "/tournaments"' do
-    expect(described_class.path(:tournaments)).to eq('/tournaments')
+    expect(routes.path(:tournaments)).to eq('/tournaments')
   end
 
   it 'generates "/tournaments/1"' do
-    expect(described_class.path(:tournament, id: 1)).to eq('/tournaments/1')
+    expect(routes.path(:tournament, id: 1)).to eq('/tournaments/1')
   end
 
   it 'generates "/tournaments/new"' do
-    expect(described_class.path(:new_tournament)).to eq('/tournaments/new')
+    expect(routes.path(:new_tournament)).to eq('/tournaments/new')
   end
 
   it 'generates "/tournaments/:id/rounds/:number"' do
     expected = '/tournaments/1/rounds/2'
-    expect(described_class.path(:round, tournament_id: 1, number: 2)).to eq(expected)
+    expect(routes.path(:round, tournament_id: 1, number: 2)).to eq(expected)
   end
 
   it 'generates "/tournaments/:id/rounds/:number/edit"' do
     expected = '/tournaments/1/rounds/2/edit'
-    expect(described_class.path(:edit_round, tournament_id: 1, number: 2)).to eq(expected)
+    expect(routes.path(:edit_round, tournament_id: 1, number: 2)).to eq(expected)
   end
 
   it 'generates "/tournaments/:tournament_id/players/:id"' do
     expected = '/tournaments/1/players/2'
-    expect(described_class.path(:player, tournament_id: 1, id: 2)).to eq(expected)
+    expect(routes.path(:player, tournament_id: 1, id: 2)).to eq(expected)
   end
 end
