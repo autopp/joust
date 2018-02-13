0f7ed0482dc66408228923dd2a9b27f57090e027
autopp <autopp.inc@gmail.com>
2018-02-13 09:47:23 +0900

    test: add test case for Controlloers::Player::Update

--- spec/web/controllers/player/update_spec.rb
+++ spec/web/controllers/player/update_spec.rb
@@ -1,10 +1,24 @@
 RSpec.describe Web::Controllers::Player::Update, type: :action do
   let(:action) { described_class.new(interactor: interactor) }
-  let(:params) { Hash[] }
+  let(:params) { { tournament_id: 1, id: 2 } }
   let(:interactor) { instance_double(Interactors::DropPlayer) }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  context 'when interactor succeeds' do
+    before do
+      result = double('interactor result')
+      allow(result).to receive(:success?).and_return(true)
+      allow(result).to receive(:errors).and_return([])
+      allow(interactor).to receive(:call).with(params).and_return(result)
+    end
+
+    it 'is successful' do
+      response = action.call(params)
+      expect(response[0]).to eq 200
+    end
+
+    it 'exposes no error' do
+      action.call(params)
+      expect(action.exposures).to include(errors: [])
+    end
   end
 end
