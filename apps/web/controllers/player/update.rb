fa81d34d4eeb58eca5bec48e3d9fe23f416ca668
autopp <autopp.inc@gmail.com>
2018-02-16 09:55:22 +0900

    feat: impl Player::Update for success case

--- apps/web/controllers/player/update.rb
+++ apps/web/controllers/player/update.rb
@@ -2,11 +2,15 @@ module Web::Controllers::Player
   class Update
     include Web::Action
 
+    expose :player
+
     def initialize(interactor: Interactors::DropPlayer.new)
       @interactor = interactor
     end
 
     def call(params)
+      result = @interactor.call(params)
+      @player = result.player
     end
   end
 end
