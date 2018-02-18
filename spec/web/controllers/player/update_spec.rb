532b90536ee8ebf12de48cd527b0981874c8aadb
autopp <autopp.inc@gmail.com>
2018-02-18 13:14:37 +0900

    feat: impl Player::Update for failure case

--- spec/web/controllers/player/update_spec.rb
+++ spec/web/controllers/player/update_spec.rb
@@ -32,4 +32,26 @@ RSpec.describe Web::Controllers::Player::Update, type: :action do
       expect(action.exposures).to include(player: player)
     end
   end
+
+  context 'when interactor fails' do
+    let(:player) { Player.new(id: 2, tournament_id: 1, name: 'player1') }
+
+    before do
+      result = double('interactor result')
+      allow(result).to receive(:success?).and_return(false)
+      allow(result).to receive(:errors).and_return(%w[error])
+
+      expected_args = satisfying { |o| o.to_h == params }
+      allow(interactor).to receive(:call).with(expected_args).and_return(result)
+    end
+
+    it 'is failure' do
+      expect(subject[0]).to eq 400
+    end
+
+    it 'exposes interactor\'s error' do
+      subject
+      expect(action.exposures).to include(errors: %w[error])
+    end
+  end
 end
