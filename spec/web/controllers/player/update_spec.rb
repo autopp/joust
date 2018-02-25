3fe3acc9a7094ce9eb9696545dd4f8218c85663f
autopp <autopp.inc@gmail.com>
2018-02-25 18:47:40 +0900

    test: assert about of tournament

--- spec/web/controllers/player/update_spec.rb
+++ spec/web/controllers/player/update_spec.rb
@@ -1,21 +1,27 @@
 RSpec.describe Web::Controllers::Player::Update, type: :action do
-  let(:action) { described_class.new(drop_player: interactor) }
+  let(:action) { described_class.new(drop_player: drop_player, find_tournament: find_tournament) }
   let(:params) { { tournament_id: 1, id: 2 } }
-  let(:interactor) { instance_double(Interactors::DropPlayer) }
+  let(:drop_player) { instance_double(Interactors::DropPlayer) }
+  let(:find_tournament) { instance_double(Interactors::FindTournament) }
 
   subject { action.call(params) }
 
   context 'when interactor succeeds' do
     let(:player) { Player.new(id: 2, tournament_id: 1, name: 'player1') }
+    let(:tournament) { Tournament.new(id: 1, name: 'My Tournament') }
 
     before do
-      result = double('interactor result')
-      allow(result).to receive(:success?).and_return(true)
-      allow(result).to receive(:errors).and_return([])
-      allow(result).to receive(:player).and_return(player)
+      drop_result = double('DropPlayer result')
+      allow(drop_result).to receive(:success?).and_return(true)
+      allow(drop_result).to receive(:errors).and_return([])
+      allow(drop_result).to receive(:player).and_return(player)
 
       expected_args = satisfying { |o| o.to_h == params }
-      allow(interactor).to receive(:call).with(expected_args).and_return(result)
+      allow(drop_player).to receive(:call).with(expected_args).and_return(drop_result)
+
+      find_result = double('FindPlayer result')
+      allow(find_result).to receive(:tournament).and_return(tournament)
+      allow(find_tournament).to receive(:call).with(id: 1).and_return(find_result)
     end
 
     it 'is successful' do
@@ -31,6 +37,11 @@ RSpec.describe Web::Controllers::Player::Update, type: :action do
       subject
       expect(action.exposures).to include(player: player)
     end
+
+    it 'exposes the tournament' do
+      subject
+      expect(action.exposures).to include(tournament: tournament)
+    end
   end
 
   context 'when interactor fails' do
@@ -42,7 +53,7 @@ RSpec.describe Web::Controllers::Player::Update, type: :action do
       allow(result).to receive(:errors).and_return(errors)
 
       expected_args = satisfying { |o| o.to_h == params }
-      allow(interactor).to receive(:call).with(expected_args).and_return(result)
+      allow(drop_player).to receive(:call).with(expected_args).and_return(result)
     end
 
     it 'is failure' do
