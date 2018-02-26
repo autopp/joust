87a4b2e859c513c584856da40bb36dfbb87dbf41
autopp <autopp.inc@gmail.com>
2018-02-26 09:47:49 +0900

    feat: impl Views::Player::Update by including module

--- apps/web/views/player/update.rb
+++ apps/web/views/player/update.rb
@@ -1,7 +1,10 @@
+require_relative '../show_tournament'
+
 module Web::Views::Player
   class Update
     include Web::View
 
+    include Web::Views::ShowTournament
     template 'tournament/show'
   end
 end
