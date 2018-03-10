239cdf05c404a3730423b8d61ac56538e9769088
autopp <autopp.inc@gmail.com>
2018-03-10 23:21:38 +0900

    feat: CreateRound depends on TournamentRepository & ScoreRepository

--- lib/joust/interactors/create_round.rb
+++ lib/joust/interactors/create_round.rb
@@ -3,5 +3,10 @@ require 'hanami/interactor'
 module Interactors
   class CreateRound
     include Hanami::Interactor
+
+    def initialize(tournament_repo: TournamentRepository.new, score_repo: ScoreRepository)
+      @tournament_repo = tournament_repo
+      @score_repo = score_repo
+    end
   end
 end
