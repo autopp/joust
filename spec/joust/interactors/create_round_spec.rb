6630913b9ad00edb6dfe4d063e944f1e3e2385b5
autopp <autopp.inc@gmail.com>
2018-03-11 15:53:37 +0900

    test: initialize test for CreateRound

--- spec/joust/interactors/create_round_spec.rb
+++ spec/joust/interactors/create_round_spec.rb
@@ -1,2 +1,10 @@
 describe Interactors::CreateRound do
+  let(:interactor) do
+    described_class.new(tournament_repo: tournament_repo, score_repo: score_repo)
+  end
+
+  let(:tournament_repo) { instance_double(TournamentRepository) }
+  let(:score_repo) { instance_double(ScoreRepository) }
+
+  subject { interactor.call(params) }
 end
