067fce0b731ef04319d7725a9d1cec933e94d4d3
autopp <autopp.inc@gmail.com>
2018-03-13 09:46:25 +0900

    feat: add param about of interactor to Controlloers::Round::Create#initilize

--- apps/web/controllers/round/create.rb
+++ apps/web/controllers/round/create.rb
@@ -2,6 +2,10 @@ module Web::Controllers::Round
   class Create
     include Web::Action
 
+    def initialize(create_round: Interactors::CreateRound.new)
+      @create_round = create_round
+    end
+
     def call(params)
     end
   end
