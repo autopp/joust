3e3c1efbce54202ccea94aae98925b2b0871d353
autopp <autopp.inc@gmail.com>
2018-03-17 17:45:36 +0900

    feat: validate params of Interactors::CreateRound

--- lib/joust/interactors/create_round.rb
+++ lib/joust/interactors/create_round.rb
@@ -11,5 +11,23 @@ module Interactors
 
     def call(_params)
     end
+
+    def valid?(params)
+      result = Validator.new(params).validate
+      if result.success?
+        true
+      else
+        result.errors.each { |e| error(e) }
+        false
+      end
+    end
+
+    class Validator
+      include Hanami::Validations
+
+      validations do
+        required(:tournament_id) { format?(/\A[0-9]+\z/) }
+      end
+    end
   end
 end
