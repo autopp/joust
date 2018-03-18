2da7aff595ced291092d5437d7bd2f6a2d27cd3f
autopp <autopp.inc@gmail.com>
2018-03-18 21:57:01 +0900

    test: add case for not existing tournament

--- spec/joust/interactors/create_round_spec.rb
+++ spec/joust/interactors/create_round_spec.rb
@@ -8,15 +8,29 @@ describe Interactors::CreateRound do
 
   subject { interactor.call(params) }
 
+  shared_examples 'failure case' do
+    it 'fails' do
+      expect(subject).not_to be_a_success
+      expect(subject.errors).not_to be_empty
+    end
+  end
+
   context 'when params are valid' do
+    let(:tournament_id) { '1' }
+    let(:params) { { tournament_id: tournament_id } }
+
+    context 'when the tournament dose not exist' do
+      before do
+        allow(tournament_repo).to receive(:find).with(id: tournament_id).and_return(nil)
+      end
+
+      it_behaves_like 'failure case'
+    end
   end
 
   context 'when params are invalid' do
     let(:params) { {} }
 
-    it 'fails' do
-      expect(subject).not_to be_a_success
-      expect(subject.errors).not_to be_empty
-    end
+    it_behaves_like 'failure case'
   end
 end
