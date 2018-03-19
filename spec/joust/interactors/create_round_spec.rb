0e450c12170c466885aecbcd54a7f6177f2cb9bf
autopp <autopp.inc@gmail.com>
2018-03-19 09:49:28 +0900

    test: add params of CreateRound

--- spec/joust/interactors/create_round_spec.rb
+++ spec/joust/interactors/create_round_spec.rb
@@ -1,9 +1,12 @@
 describe Interactors::CreateRound do
   let(:interactor) do
-    described_class.new(tournament_repo: tournament_repo, score_repo: score_repo)
+    described_class.new(
+      tournament_repo: tournament_repo, score_repo: score_repo, find_tournament: find_tournament
+    )
   end
 
   let(:tournament_repo) { instance_double(TournamentRepository) }
+  let(:find_tournament) { instance_double(Interactors::FindTournament) }
   let(:score_repo) { instance_double(ScoreRepository) }
 
   subject { interactor.call(params) }
@@ -19,9 +22,14 @@ describe Interactors::CreateRound do
     let(:tournament_id) { '1' }
     let(:params) { { tournament_id: tournament_id } }
 
+    before do
+      allow(find_tournament).to receive(:call).with(id: tournament_id).and_return(find_result)
+    end
+
     context 'when the tournament dose not exist' do
       before do
-        allow(tournament_repo).to receive(:find).with(id: tournament_id).and_return(nil)
+        allow(find_result).to receive(:success?).and_return(false)
+        allow(find_result).to receive(:errors).and_return(['error of FindTournament'])
       end
 
       it_behaves_like 'failure case'
