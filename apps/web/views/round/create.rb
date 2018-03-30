470678d6f61d2108914e991cb6fdd04a3665f3c3
autopp <autopp.inc@gmail.com>
2018-03-30 09:44:44 +0900

    feat: impl View::Round::Create

--- apps/web/views/round/create.rb
+++ apps/web/views/round/create.rb
@@ -1,5 +1,10 @@
+require_relative '../show_tournament'
+
 module Web::Views::Round
   class Create
     include Web::View
+
+    include Web::Views::ShowTournament
+    template 'tournament/show'
   end
 end
