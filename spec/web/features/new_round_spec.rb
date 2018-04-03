a8a3ac52647e18e7dd8a48bb624ce74b5d99f4e0
autopp <autopp.inc@gmail.com>
2018-04-03 09:48:36 +0900

    test: add feature test about of creating round

---
+++ spec/web/features/new_round_spec.rb
@@ -0,0 +1,27 @@
+require 'features_helper'
+
+feature 'Create new round' do
+  before do
+    players = <<~INPUT
+      player1
+      player2
+      player3
+      player4
+    INPUT
+    input = {
+      tournament: {
+        name: 'My Tournament', players: players,
+        total_vp_used: true, rank_history_used: true
+      }
+    }
+    result = Interactors::CreateTournament.new.call(input)
+    @id = result.tournament.id
+  end
+
+  scenario 'Visit a tournament page and create new round' do
+    visit Web.routes.path(:tournament, id: @id)
+    click_on 'Start new round'
+
+    expect(current_path).to match(%r{/tournaments/#{@id}/rounds/\d+/edit})
+  end
+end
