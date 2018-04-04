6c2dd7a73f871a684ef240dea3b78599d13cb6c5
autopp <autopp.inc@gmail.com>
2018-04-04 09:44:23 +0900

    test: add spec of Views::Round::Edit

--- spec/web/views/round/edit_spec.rb
+++ spec/web/views/round/edit_spec.rb
@@ -1,6 +1,15 @@
+require_relative '../views_helper.rb'
+require_relative '../../../../apps/web/views/round/edit'
+
 RSpec.describe Web::Views::Round::Edit, type: :view do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) do
+    tournament = Tournament.new(id: 1, name: 'My Tournament')
+    { round: Round.new(id: 2, tournament_id: 1, number: 2, tournament: tournament) }
+  end
   let(:template)  { Hanami::View::Template.new('apps/web/templates/round/edit.html.erb') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
+
+  it_behaves_like 'common view', title: 'My Tournament: Round 2',
+                                 back_path: Web.routes.path(:tournament, id: 1)
 end
